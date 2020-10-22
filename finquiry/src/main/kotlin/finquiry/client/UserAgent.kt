07888ecfd5efa7f8a4af10a90cca62de983c1994
davidjung-kr <jerryjueng@daum.net>
2020-10-22 22:46:05 +0900

    firefox UserAgent 추가

--- finquiry/src/main/kotlin/finquiry/client/UserAgent.kt
+++ finquiry/src/main/kotlin/finquiry/client/UserAgent.kt
@@ -1,5 +1,7 @@
 package finquiry.client
 
 class UserAgent {
-    var firefox:Array<String> = arrayOf("", "", "", "")
+    var firefox:Array<String> = arrayOf(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0",
+            "", "", "")
 }
